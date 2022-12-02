23bb69586d31a640c010b1b37e50f10b6ca269c5
Fred Fettinger <fettinge@gmail.com>
2022-12-02 04:21:06 -0600

    day01

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,12 +1,29 @@
 use advent_of_code::{create_runner, named, Named, Runner};
+use itertools::Itertools;
 use std::str::Lines;
 
 fn part1(input: Lines) -> String {
-    input.count().to_string()
+    input
+        .map(|line| line.parse::<i32>().ok())
+        .group_by(|x| x.is_some())
+        .into_iter()
+        .filter_map(|(_, grp)| grp.while_some().sum1())
+        .max()
+        .unwrap_or(0)
+        .to_string()
 }
 
 fn part2(input: Lines) -> String {
-    input.count().to_string()
+    input
+        .map(|line| line.parse::<i32>().ok())
+        .group_by(|x| x.is_some())
+        .into_iter()
+        .filter_map(|(_, grp)| grp.while_some().sum1())
+        .map(|x: i32| -x)
+        .k_smallest(3)
+        .sum::<i32>()
+        .abs()
+        .to_string()
 }
 
 fn main() {
@@ -24,7 +41,7 @@ mod tests {
     #[test]
     fn example() {
         let input = include_str!("example.txt");
-        verify!(part1, input, "1");
-        verify!(part2, input, "1");
+        verify!(part1, input, "24000");
+        verify!(part2, input, "45000");
     }
 }
