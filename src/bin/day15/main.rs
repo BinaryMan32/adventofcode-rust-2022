d482be72c208847bb2f449151b0d7583d51fbafa
Fred Fettinger <fettinge@gmail.com>
2023-01-16 18:21:59 -0600

    day15 part2

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -1,7 +1,7 @@
 use advent_of_code::{create_runner, named, Named, Runner};
 use itertools::Itertools;
 use lazy_regex::regex_captures;
-use std::{ops::Range, str::Lines};
+use std::{iter::once, ops::Range, str::Lines};
 
 #[derive(Debug, PartialEq)]
 struct Pos {
@@ -16,6 +16,20 @@ impl Pos {
 
     fn manhattan_distance(&self, other: &Pos) -> isize {
         (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+struct Params {
+    row: isize,
+    search: isize,
+}
+
+impl Params {
+    fn parse(line: &str) -> Self {
+        let (_, row, search) = regex_captures!(r"Params row=(\d+) search=(\d+)", line).unwrap();
+        let row = row.parse::<isize>().unwrap();
+        let search = search.parse::<isize>().unwrap();
+        Params { row, search }
     }
 }
 
@@ -68,7 +82,6 @@ fn add_range(existing_ranges: Vec<Range<isize>>, mut new_range: Range<isize>) ->
         .into_iter()
         .filter(|existing_range| {
             if let Some(combined) = combined_range(existing_range, &new_range) {
-                println!("combining a={existing_range:?} b={new_range:?} c={combined:?}");
                 new_range = combined;
                 false
             } else {
@@ -80,8 +93,31 @@ fn add_range(existing_ranges: Vec<Range<isize>>, mut new_range: Range<isize>) ->
     result
 }
 
+fn invert_ranges(ranges: Vec<Range<isize>>, end: isize) -> Vec<Range<isize>> {
+    let mut ranges = ranges
+        .into_iter()
+        .filter(|r| r.end >= 1 && r.start < end)
+        .collect_vec();
+    ranges.sort_by(|a, b| a.start.cmp(&b.start));
+    if let Some(r) = ranges.first_mut() {
+        r.start = r.start.max(0)
+    }
+    if let Some(r) = ranges.last_mut() {
+        r.end = r.end.min(end)
+    }
+    let ranges = once(0..0)
+        .chain(ranges.into_iter())
+        .chain(once(end..end))
+        .collect_vec();
+    ranges
+        .iter()
+        .zip(ranges[1..].iter())
+        .map(|(a, b)| a.end..b.start)
+        .collect_vec()
+}
+
 fn part1(mut input: Lines) -> String {
-    let row = input.next().unwrap().parse::<isize>().unwrap();
+    let row = Params::parse(input.next().unwrap()).row;
     println!("row={row}");
     let sensors = input.into_iter().flat_map(Sensor::parse).collect_vec();
     let beacon_scan_size = sensors
@@ -106,8 +142,26 @@ fn part1(mut input: Lines) -> String {
     (beacon_scan_size - beacons_in_row).to_string()
 }
 
-fn part2(input: Lines) -> String {
-    input.take(0).count().to_string()
+fn part2(mut input: Lines) -> String {
+    let search_max = Params::parse(input.next().unwrap()).search;
+    println!("search_max={search_max}");
+
+    let sensors = input.into_iter().flat_map(Sensor::parse).collect_vec();
+
+    for y in 0..search_max {
+        let no_beacon_ranges = sensors
+            .iter()
+            .flat_map(|s| s.scan_range_for_row(y))
+            .fold(Vec::new(), add_range);
+        for mut range in invert_ranges(no_beacon_ranges, search_max + 1) {
+            if let Some(x) = range.next() {
+                println!("x={x} y={y}");
+                return (x * 4000000 + y).to_string();
+            }
+        }
+    }
+
+    "".to_string()
 }
 
 fn main() {
@@ -123,7 +177,7 @@ mod tests {
     use advent_of_code::verify;
 
     #[test]
-    fn parse() {
+    fn parse_sensor() {
         assert_eq!(
             Sensor::parse("Sensor at x=2, y=18: closest beacon is at x=-2, y=15"),
             Some(Sensor {
@@ -142,9 +196,14 @@ mod tests {
     }
 
     #[test]
+    fn invert() {
+        assert_eq!(invert_ranges(vec![1..2, 3..5], 6), vec![0..1, 2..3, 5..6]);
+    }
+
+    #[test]
     fn example() {
         let input = include_str!("example.txt");
         verify!(part1, input, "26");
-        verify!(part2, input, "0");
+        verify!(part2, input, "56000011");
     }
 }
